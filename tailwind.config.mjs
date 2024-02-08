2c6ede71315b60a7f72797d32b2be92697c8d0f2
Gary Aspinall <garyfaspinall@gmail.com>
2024-02-08 16:35:38 +1000

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+      'lime': '#8cae36',
+      'green1': '#3E6A31',
+      'white': '#FFFFFF',
+      'off-white': '#FCF8ED',
+    },
+		extend: {},
+	},
+	plugins: [],
+}
